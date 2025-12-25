11dcbcb866815a3e4dacc19674d3bb471aa9f620
sukhbirkaushall <113819281+sukhbirkaushall@users.noreply.github.com>
2025-12-25 10:38:18 +0530

    Update Feed.jsx

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -9,7 +9,7 @@ const Feed = ({ category }) => {
     const [data, setData] = useState([])
 
     const fetchData = async () => {
-        const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&videoCategoryId=${category}&key=${API_KEY}`
+        const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
         await fetch(videoList_url).then(response => response.json()).then(data => setData(data.items))
     }
 
